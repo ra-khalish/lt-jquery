61ed0dffbd070fc9a9ba52813173102860df9a82
Rafi Khalish <rafidev30@gmail.com>
2019-12-04 17:42:39 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus aspernatur aperiam velit libero ea perspiciatis, minus eveniet cupiditate similique, iste repellendus nulla fugiat culpa laudantium sequi, error repudiandae mollitia?</p>
+    <ul>
+        <li>Cilupbaa</li>
+    </ul>
 </body>
 </html>
