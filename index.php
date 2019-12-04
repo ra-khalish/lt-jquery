262a2bab5f601e1315a8e4d68b277a96e4523918
Rafi Khalish <rafidev30@gmail.com>
2019-12-04 17:37:14 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus aspernatur aperiam velit libero ea perspiciatis, minus eveniet cupiditate similique, iste repellendus nulla fugiat culpa laudantium sequi, error repudiandae mollitia?</p>
 </body>
 </html>
